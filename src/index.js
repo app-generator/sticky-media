315e45aa77dc4f4e43263ffd8d80e502feedb5b2
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-15 18:25:34 +0200

    Release v0.0.7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* Stycky Media - v0.0.6
+* Stycky Media - v0.0.7
 =========================================================
 * Product Page: https://github.com/app-generator/sticky-media
 * Copyright AppSeed (https://appseed.us)
